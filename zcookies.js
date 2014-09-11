d55b42bb417dd6c0e333b071d33f904781d0871a
ukyo <ukyo.web@gmail.com>
2014-09-11 20:57:13 +0900

    first commit!

---
+++ zcookies.js
@@ -0,0 +1,18 @@
+// zcookies 0.0.1 is released under the MIT Lisence
+(function(global) {
+  "use strict";
+
+  global.zcookies = {
+    get: function(k) {
+      return msgpack.unpack(pako.inflateRaw(atob(Cookies.get(k))));
+    },
+    set: function(k, v, options) {
+      var x = Cookies.get(k);
+      var y = btoa(String.fromCharCode.apply(null, pako.deflateRaw(msgpack.pack(v), {level: 9})));
+      Cookies.set(k, y, options);
+      if (x !== y && x === Cookies.get(k)) {
+        throw new Error('MiniCookies: A too large value!');
+      }
+    }
+  };
+})(this);
